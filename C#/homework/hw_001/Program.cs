fdd92463a8ef85ca68bdf9881b7d950e8ebe9c52
diadiawowka <v.kucinsky@gmail.com>
2022-09-25 16:54:19 +0300

    red

--- C#/homework/hw_001/Program.cs
+++ C#/homework/hw_001/Program.cs
@@ -18,27 +18,6 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 333;
 
-// первый вариант
-
-// int max = a1;
-// if(b1 > max) max = b1;
-// if(c1 > max) max = c1;
-
-// if(a2 > max) max = a2;
-// if(b2 > max) max = b2;
-// if(c2 > max) max = c2;
-
-// if(a3 > max) max = a3;
-// if(b3 > max) max = b3;
-// if(c3 > max) max = c3;
-
-// Второй варивнт
-// int max1 = Max(a1,b1,c1);
-// int max2 = Max(a2,b2,c2);
-// int max3 = Max(a3,b3,c3);
-// int max = Max(max1, max2, max3);
-
-// третий вариант
 int max = Max(Max(a1,b1,c1), Max(a2,b2,c2), Max(a3,b3,c3));
 
 Console.WriteLine(max);
