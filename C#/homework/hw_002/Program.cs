771a2d3fa8ac1cc75d185e307750d6960fbd7711
diadiawowka <v.kucinsky@gmail.com>
2022-10-27 17:39:31 +0300

    sfg

--- C#/homework/hw_002/Program.cs
+++ C#/homework/hw_002/Program.cs
@@ -48,39 +48,109 @@
 // 5 2 6 7
 
 
+// void PrintArray(int[,] array)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void Sum(int [,] array)
+// {
+
+//     int stringsum = Int32.MaxValue;
+//     int indexstring = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     int sum = 0;
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         sum = sum+ array[i, j];        
+//     }
+//     if (sum < stringsum)
+//     {
+//         stringsum = sum;
+//         indexstring++;
+//     }
+// }
+// Console.WriteLine($"Строка с наименьшей суммой Nr: {indexstring}, сумма = {stringsum}");
+// }
+
+// int[,] array = {{ 0, 4, 6, 0 },{3, 7, 2, 5},{1, 6, 3, 6},{1, 6, 3, 7}};
+// PrintArray(array);    
+// Sum(array);  
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+void FillArray(int[,] array )
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
 void PrintArray(int[,] array)
 {
-    Console.WriteLine();
+    Console.WriteLine("first: ");
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]}");
+            Console.Write($"{array[i, j]},");
         }
         Console.WriteLine();
     }
 }
-void Sum(int [,] array)
+void FillArray2(int[,] array2)
 {
-
-    int stringsum = Int32.MaxValue;
-    int indexstring = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
+    for(int i = 0; i < array2.GetLength(0); i++)
+    {
+        for(int j = 0; j < array2.GetLength(1); j++)
+        {
+            array2[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray2(int[,] array2)
+{
+    Console.WriteLine("second: ");
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            Console.Write($"{array2[i, j]},");
+        }
+        Console.WriteLine();
+    }
+}
+void Summ(int[,] array, int [,] array2)
 {
     int sum = 0;
-    for (int j = 0; j < array.GetLength(1); j++)
+    Console.WriteLine();
+    Console.WriteLine("Произведение двух матриц: ");
+    Console.WriteLine();
+    for(int i = 0; i < array.GetLength(0); i++)
     {
-        sum = sum+ array[i, j];        
-    }
-    if (sum < stringsum)
-    {
-        stringsum = sum;
-        indexstring++;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = array[i, j] * array2[i, j];
+            Console.Write($"{sum},");
+        }
+        Console.WriteLine();
     }
 }
-Console.WriteLine($"Строка с наименьшей суммой Nr: {indexstring}, сумма = {stringsum}");
-}
-
-int[,] array = {{ 0, 4, 6, 0 },{3, 7, 2, 5},{1, 6, 3, 6},{1, 6, 3, 7}};
-PrintArray(array);    
-Sum(array);  
+int[,] array = new int[4, 2];
+int[,] array2 = new int[4, 2];
+FillArray(array);
+PrintArray(array);
+FillArray2(array2);
+PrintArray2(array2);
+Summ(array, array2);
